f6278cc1e801bb0b2468163b40646da878351216
MauryaAbhi378 <abhim0505@gmail.com>
2026-05-04 15:12:20 +0530

    Update the experience

--- experience.js
+++ experience.js
@@ -3,7 +3,7 @@ const experiences = [
     companyName: "CometChat",
     jobTitle: "Trainee Engineer",
     startDate: "2025-10",
-    endDate: "Present",
+    endDate: "2026-4",
     description:
       "Resolved high-priority developer tickets, improving integration efficiency through deep troubleshooting in React, Next.js. Implemented product features including Mark as Unread and a share message supporting all message types across both React and React Native platforms. Developed and maintained the Chat UI Kit, delivering customizable and responsive components that improved developer experience and end-user engagement.",
     techLearned: ["Next.js", "React-Native", "React"],
@@ -13,9 +13,9 @@ const experiences = [
   },
   {
     companyName: "Frontendone",
-    jobTitle: "Junior Frontend Developer",
+    jobTitle: "Frontend Developer Intern",
     startDate: "2025-05",
-    endDate: "2025-09",
+    endDate: "2025-08",
     description:
       "Developed and maintained web applications using HTML, CSS, and JavaScript.Collaborated with cross-functional teams to deliver high-quality software on time Design & Developed multiple websites for clients.",
     techLearned: ["TypeScript", "React", "Git"],
